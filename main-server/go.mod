f46691b43fcfde81efe78a1361105b89603ac0a4
Fede Martino <fedemartino@gmail.com>
2025-07-04 23:09:26 -0300

    version 1 of go http server

---
+++ main-server/go.mod
@@ -0,0 +1,3 @@
+module fedemartino/home-server
+
+go 1.24.4
---
+++ main-server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	t, _ := template.ParseFiles("html/index.html")
+	t.Execute(w, "")
+}
+
+func main() {
+	var port = ":8080"
+
+	// Serve files from the "static" directory
+	jsFileServer := http.FileServer(http.Dir("./html/js"))
+	imgFileServer := http.FileServer(http.Dir("./html/img"))
+	http.Handle("/js/", http.StripPrefix("/js/", jsFileServer))
+	http.Handle("/img/", http.StripPrefix("/img/", imgFileServer))
+
+	//handle dynamic urls
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/home/", homeHandler)
+
+	fmt.Println("Listening on http://localhost" + port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
